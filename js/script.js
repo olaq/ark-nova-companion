1426fd96f234d4379f979008110d73f35676ba76
molaq <molaq@github.com>
2023-12-16 23:12:49 +0100

    Add favicon and manifest files, implement service worker for PWA

--- js/script.js
+++ js/script.js
@@ -83,6 +83,14 @@ setInterval(function () {
 }, 5000);
 
 
-
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  }
 
 
